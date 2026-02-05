2063ea9c69eb47e479b86780a5e95dab593c45a9
Matheus Alves <rdptestuser61@gmail.com>
2026-02-05 01:13:20 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/motorola/manaus
+DEVICE_PATH := device/motorola/edge40neo
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
