99bf8ad3a6fc92fbe438d943e194031732311e32
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-05 20:38:30 -0300

    Update twrp_edge40neo.mk

--- twrp_edge40neo.mk
+++ twrp_edge40neo.mk
@@ -19,7 +19,7 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/motorola/edge40neo/
 PRODUCT_DEVICE := edge40neo
 PRODUCT_NAME := twrp_edge40neo
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := motorola (2022)
+PRODUCT_MODEL := motorola Edge (2022)
 PRODUCT_MANUFACTURER := motorola
 # Assert that the device is edge40neo
 TARGET_OTA_ASSERT_DEVICE := edge40neo,manaus
