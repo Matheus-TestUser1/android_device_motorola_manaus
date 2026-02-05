00d7ded5a2c90a5188b0aeac754a2fa4f1d78fc5
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-04 21:47:22 -0300

    Update AndroidProducts.mk for new product configuration

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_manaus.mk
+    $(LOCAL_DIR)/omni_manaus.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_manaus-user \
-    twrp_manaus-userdebug \
-    twrp_manaus-eng
+    omni_manaus-eng \
+    omni_manaus-userdebug
