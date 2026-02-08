45a83662be6807a36ea31479bfc5026f9e485f0b
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-08 13:59:22 -0300

    Atualizar o BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -262,7 +262,8 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 # PROPRIEDADES
 # ============================================================================
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true 
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+ BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true 
 # ============================================================================
 # DEBUG
 # ============================================================================
