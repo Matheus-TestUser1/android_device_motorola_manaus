7609a5a27ff55a28ed34c80726dc08741e6003ba
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-08 16:00:19 -0300

    Delete bootimg.mk

--- bootimg.mk
+++
@@ -1,6 +0,0 @@
-# bootimg.mk - Minimal para vendor_boot v4
-
-LOCAL_PATH := $(call my-dir)
-
-# Não precisa de regras complexas, o sistema de build do TWRP já lida com vendor_boot v4
-# Este arquivo só é necessário se você precisar de customizações especiais
