1d6f67b5fc433a250e82221eef68e6685a485653
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-08 12:11:46 -0300

    Update and rename twrp_edge40neo.mk to twrp_manaus.mk

--- twrp_manaus.mk
+++ twrp_manaus.mk
@@ -14,12 +14,11 @@ $(call inherit-product, device/motorola/edge40neo/device.mk)
 
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/motorola/edge40neo/recovery/root,recovery/root)
-
-# Device identifier
-PRODUCT_DEVICE := edge40neo
-PRODUCT_NAME := twrp_edge40neo
+# Device identifier - USAR CODENAME REAL
+PRODUCT_DEVICE := manaus
+PRODUCT_NAME := twrp_manaus
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := motorola Edge (2022)
+PRODUCT_MODEL := motorola edge 40 neo
 PRODUCT_MANUFACTURER := motorola
-# Assert that the device is edge40neo
-TARGET_OTA_ASSERT_DEVICE := edge40neo
+
+TARGET_OTA_ASSERT_DEVICE := manaus,manaus_g,edge40neo
