85f344e346c862a37279d05ba245e5ba774266a9
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-02-08 15:52:49 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 TeamWin Recovery Project
 #
@@ -8,30 +7,38 @@
 DEVICE_PATH := device/motorola/manaus
 
 # ============================================================================
-# CONFIGURAÇÕES BÁSICAS
+# BASIC
 # ============================================================================
 ALLOW_MISSING_DEPENDENCIES := true
+
 TARGET_BOOTLOADER_BOARD_NAME := manaus
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
+
 BOARD_VENDOR := motorola
 TARGET_SOC := mt6879
+TARGET_BOARD_PLATFORM := mt6879
+TARGET_BOARD_PLATFORM_GPU := mali-g57mc2
+
+BOARD_USES_MTK_HARDWARE := true
+MTK_HARDWARE := true
 
 # ============================================================================
-# VENDOR_BOOT V4 - CONFIGURAÇÕES CRÍTICAS (NO TOPO!)
+# VENDOR_BOOT / BOOT HEADER (V4)
 # ============================================================================
-BOARD_USES_VENDOR_BOOT := true
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_USES_VENDOR_BOOTIMAGE := true
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 
 # ============================================================================
-# ARQUITETURA
+# ARCH
 # ============================================================================
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a78
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
@@ -41,25 +48,8 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 # ============================================================================
-# PLATAFORMA
+# KERNEL / MKBOOTIMG
 # ============================================================================
-TARGET_BOARD_PLATFORM := mt6879
-TARGET_BOARD_PLATFORM_GPU := mali-g57mc2
-BOARD_USES_MTK_HARDWARE := true
-MTK_HARDWARE := true
-
-
-# ============================================================================
-# KERNEL
-# ============================================================================
-# vendor_boot must exist
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-
-# only move resources when vendor_boot exists
-ifneq ($(strip $(BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE)),)
-  BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-endif
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x11088000
@@ -73,48 +63,50 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
+
 BOARD_RAMDISK_USE_LZ4 := true
 LZMA_RAMDISK_TARGETS := recovery
 
-# Pré-compilados
+# Prebuilts
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
-# Recovery ramdisk do vendor_boot stock
-BOARD_PREBUILT_RECOVERY_RAMDISK := $(DEVICE_PATH)/prebuilt/ramdisk.cpio
-
-# Argumentos mkbootimg
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+# mkbootimg args
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
 # ============================================================================
-# PARTIÇÕES
+# PARTITIONS / DYNAMIC
 # ============================================================================
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
 BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm
 BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
 # ============================================================================
-# SISTEMA DE ARQUIVOS
+# FS TYPES (EROFS CONFIG)
 # ============================================================================
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EROFS := true
+
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := erofs
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_PRODUCT := product
@@ -132,6 +124,7 @@ BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_VBMETA_SYSTEM_ARGS += --flags 3
 
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
@@ -144,14 +137,16 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
+
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
 TW_PREPARE_DATA_MEDIA_EARLY := true
 BOARD_USES_METADATA_PARTITION := true
 BOARD_ROOT_EXTRA_FOLDERS += metadata
 
 # ============================================================================
-# A/B
+# A/B OTA
 # ============================================================================
 AB_OTA_UPDATER := true
 TW_INCLUDE_REPACKTOOLS := true
@@ -172,9 +167,11 @@ AB_OTA_PARTITIONS := \
 # USB
 # ============================================================================
 TARGET_USB_CONTROLLER := 11201000.usb0
+
 TW_USB_VENDOR_ID := 0x22b8
 TW_USB_PRODUCT_ID := 0x2e81
 TW_USB_PRODUCT_ID_FASTBOOT := 0x2e80
+
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 
@@ -184,7 +181,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_NO_SCREEN_BLANK := false
+
 TW_NO_SCREEN_TIMEOUT := true
 TW_DEVICE_VERSION := manaus_MT6879_stable
 
@@ -194,41 +191,33 @@ TW_DEFAULT_BRIGHTNESS := 1200
 
 TW_HAS_MTP := true
 TW_MTP_DEVICE := /dev/mtp_usb
+
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_NO_USB_STORAGE := false
 
-TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_FUSE_EXFAT := true
-TW_INCLUDE_FUSE_NTFS := true
 TW_USE_NEW_MINADBD := true
 TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_LIBRESETPROP := true
+
 TW_EXCLUDE_TWRPAPP := true
 TW_EXCLUDE_APEX := true
 
-TW_NO_BATT_PERCENT := false
 TW_NO_FASTBOOT_BOOT := true
 TW_USE_FSCRYPT_POLICY := 1
+
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone3/temp
 BOARD_UFS_SUPPORT := true
-TW_OVERRIDE_SYSTEM_PROPS := "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
+
+TW_OVERRIDE_SYSTEM_PROPS := \
+  "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
 
 # ============================================================================
-# OTIMIZAÇÕES
-# ============================================================================
-TARGET_USES_64_BIT_BINDER := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-TARGET_USES_HWC2 := true
-TARGET_USES_VULKAN := true
-BOARD_VNDK_VERSION := current
-
-# ============================================================================
-# SEGURANÇA (SEM CRIPTOGRAFIA)
+# SECURITY (no crypto)
 # ============================================================================
 TW_INCLUDE_CRYPTO := false
 TW_INCLUDE_CRYPTO_FBE := false
@@ -241,13 +230,13 @@ BOARD_USES_QCOM_FBE_DECRYPTION := false
 TARGET_OTA_ASSERT_DEVICE := manaus,manaus_g,edge40neo
 
 # ============================================================================
-# VERSÕES
+# VERSIONS
 # ============================================================================
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 
 # ============================================================================
-# PROPRIEDADES
+# PROPS
 # ============================================================================
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
